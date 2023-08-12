45fad1c4741c08bcb39c605fb651341679e1953d
Pedro Dias <pedrofilipe.md@gmail.com>
2023-08-12 10:33:33 +0100

    first iteration; added procedural map generation of ground and trees; added tree collision;

---
+++ TileMap.gd
@@ -0,0 +1,79 @@
+extends TileMap
+
+var rng = RandomNumberGenerator.new()
+var count = 0
+var moisture = FastNoiseLite.new()
+var temperature = FastNoiseLite.new()
+var altitude = FastNoiseLite.new()
+var items_chance = FastNoiseLite.new()
+const CHUNK_WIDTH = 32
+const CHUNK_HEIGHT = 32
+# advised against in production; the positions of nodes may change
+@onready var player = get_parent().get_child(1)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	moisture.seed = randi()
+	temperature.seed = randi()
+	altitude.seed = randi()
+	items_chance.seed = randi()
+	items_chance.frequency = 1
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	count += 1
+	if (count % 15) == 0:
+		count = 0
+		generate_chunk(player.position)
+
+func generate_chunk(position):
+	var tile_pos = local_to_map(position)
+	for x in range(CHUNK_WIDTH):
+		for y in range(CHUNK_HEIGHT):
+			var new_x = tile_pos.x - CHUNK_WIDTH/2 + x
+			var new_y = tile_pos.y - CHUNK_HEIGHT/2 + y
+			var moist = moisture.get_noise_2d(new_x, new_y)
+			var temp = temperature.get_noise_2d(new_x, new_y)
+			var alt = altitude.get_noise_2d(new_x, new_y)
+			var chance = items_chance.get_noise_2d(new_x, new_y)
+			
+			set_tile_type_z0(Vector2i(new_x, new_y), alt, moist, temp, chance)
+			set_tile_type_z1(Vector2i(new_x, new_y), alt, moist, temp, chance)
+
+			# set_cell(0, Vector2i(new_x, new_y), 0, Vector2i(5, 1))
+
+func set_tile_type_z0(pos_vec, alt, moist, temp, chance):
+	var tile_vec = get_random_ground_vec(chance)
+	if alt <= 0.2:  # water
+		tile_vec = Vector2i(5, 6)
+	elif alt > 0.2 and alt < 0.25: # sand
+		tile_vec = Vector2i(5, 4)
+	elif alt > 0.25 and moist < 0:  # swamp
+		if alt < 0.26 and chance < 0:
+			tile_vec = Vector2i(27, 7)
+		else:
+			tile_vec = Vector2i(14, 7)
+
+	set_cell(0, pos_vec, 0, tile_vec)
+
+func get_random_ground_vec(chance):
+	# the ground tiles are a matrix from (5,0) to (6,1) of 4 tiles
+	var vec = Vector2i(5, 0)
+	if chance < -0.25:
+		vec = Vector2i(5, 0)
+	elif chance < 0.25:
+		vec = Vector2i(5, 1)
+	elif chance < 0.75:
+		vec = Vector2i(6, 0)
+	else:
+		vec = Vector2i(6, 1)
+		
+	return vec
+
+func set_tile_type_z1(pos_vec, alt, moist, temp, chance):
+	
+	if moist > 0 and alt > 0.3 and alt <= 0.4 and chance > 0:
+		var tile_vec = Vector2i(7, 3)
+		set_cell(1, pos_vec, 0, tile_vec)
+	elif moist > 0 and alt > 0.4 and chance > 0.3:
+		set_cell(1, pos_vec, 0, Vector2i(7, 0), )
---
+++ player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 200
+const ACCEL = 1000
+const FRICTION = 600
+
+var input = Vector2.ZERO
+
+func _physics_process(delta):
+	player_movement(delta)
+
+
+func get_input_axis():
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return input.normalized()
+
+	
+func player_movement(delta):
+	input = get_input_axis()
+
+	if input == Vector2.ZERO:  # no input
+		if velocity.length() > (FRICTION * delta):
+			# if we are moving we will slow down
+			velocity -= velocity.normalized() * (FRICTION * delta)
+		else:
+			velocity = Vector2.ZERO
+	else:  # moving the player
+		velocity += (input * ACCEL * delta)
+		velocity = velocity.limit_length(MAX_SPEED)
+		
+	move_and_slide()
